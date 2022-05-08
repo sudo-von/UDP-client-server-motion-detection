c05e3e982a8e32ab9b8e711ed61985be48f222ad
VoN <sudo.von.contact@gmail.com>
2022-05-07 21:00:55 -0500

    feat: :sparkles: Added udp server

---
+++ udp_server.py
@@ -0,0 +1,26 @@
+import os
+import socket
+
+class UDPServer:
+
+    def __init__(self, local_ip, local_port, buffer_size, log_server):
+        self.local_ip = local_ip
+        self.local_port = local_port
+        self.buffer_size = buffer_size
+        self.log_server = log_server
+        self.socket = None 
+
+    def open_socket(self, socket_family, socket_type):
+        self.socket = socket.socket(family=socket_family, type=socket_type)
+        self.socket.bind((self.local_ip, self.local_port))
+        self.log_server.log_socket(self.local_ip, self.local_port)
+
+    def receive_packets(self):
+        while(True):
+            bytes_address_pair = self.socket.recvfrom(self.buffer_size)
+            message, address = bytes_address_pair
+            self.log_server.log_client(message, address)
+            self.send_packets(str.encode("test"), address)
+
+    def send_packets(self, bytes_to_send, address):
+        self.socket.sendto(bytes_to_send, address)
