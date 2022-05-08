79db92e746012caf5515b77945c4e7ced9ae3b37
VoN <sudo.von.contact@gmail.com>
2022-05-07 21:02:02 -0500

    feat: :construction: Added main service, still in progress

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-
+import os
 import socket
+from udp_server import UDPServer
+from udp_server_log import UDPServerLog
+from udp_client import UDPClient
+from udp_client_log import UDPClientLog
+from motion_detection import MotionDetection
+from motion_detection_log import MotionDetectionLog
 
-localIP = "127.0.0.1"
-localPort = 20001
-bufferSize = 1024
-msgFromServer = "Hello UDP Client"
-bytesToSend = str.encode(msgFromServer)
-# Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
-print("UDP server up and listening")
-# Listen for incoming datagrams
-while(True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    print(clientMsg)
-    print(clientIP)
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
+if __name__ == '__main__':
+
+    bytes_to_send = str.encode('VoN')
+
+    server_ip = os.getenv("SERVER_IP") or "127.0.0.1"
+    server_port = os.getenv("SERVER_PORT") or 20000
+    buffer_size = os.getenv("BUFFER_SIZE") or 1024
+
+    udp_server_log = UDPServerLog()
+    udp_server = UDPServer(server_ip, server_port, buffer_size, udp_server_log)
+    udp_server.open_socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_server.receive_packets()
+
+    udp_client_log = UDPClientLog()
+    udp_client = UDPClient(server_ip, server_port, buffer_size, udp_client_log)
+    udp_client.open_socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_server.send_packets(bytes_to_send)
+
+    motion_detection_log = MotionDetectionLog()
+    motion_detection = MotionDetection(motion_detection_log)
+    motion_detection.open_camera(2)
+    motion_detection.detect_movement()
